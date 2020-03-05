f106281b8f822faa1770d2a70683e9b4e149db8f
Vasca <vasiharg@gmail.com>
2020-03-05 14:34:56 +0000

    minor changes

--- src/assets/js/views/movieDetailsView.js
+++ src/assets/js/views/movieDetailsView.js
@@ -9,21 +9,28 @@ export const clearMovieDetailsModal = () => {
 export const renderMovieDetailsModel = movieDetails => {
     console.log('here')
     const markup = `
-            <div class="poster">
-                <img class="movie-poster" src="${movieDetails.getPoster()}"/>
+        <div class="movie-details">
+            <img class="movie-details-poster" src="${movieDetails.getPoster()}"/>
+            <div>
+                <h1 class="movie-details-title"> ${movieDetails.getTitle()} </h1>
+                <div class="movie-details-info">
+                    <p id="genre"><b>Genre: </b>${movieDetails.getGenre()}</p>
+                    <p id="released"><b>Released: </b>${movieDetails.getReleased()}</p>
+                    <p id="rated"><b>Rated: </b>${movieDetails.getRated()}</p>
+                    <p id="imdbRating"><b>IMDB Rating: </b>${movieDetails.getImdbRating()}</p>
+                    <p id="director"><b>Director: </b>${movieDetails.getDirector()}</p>
+                    <p id="writer"><b>Writer: </b>${movieDetails.getWriter()}</p>
+                    <p id="actors"><b>Actors: </b>${movieDetails.getActors()}</p>
+                </div>
             </div>
-            <p class="movie-title">${movieDetails.getTitle()}</p>
-            <div class="text">
-                <p id="genre">${movieDetails.getGenre()}</p>
-                <p id="released">${movieDetails.getReleased()}</p>
-                <p id="rated">${movieDetails.getRated()}</p>
-                <p id="imdbRating">${movieDetails.getImdbRating()}</p>
-                <p id="director">${movieDetails.getDirector()}</p>
-                <p id="writer">${movieDetails.getWriter()}</p>
-                <p id="actors">${movieDetails.getActors()}</p>
-            </div>
+        </div>
+        <div class="movie-details-plot">
+            <h2 class="movie-details-title">
+                Plot
+            </h2>
             <p id="plot">${movieDetails.getPlot()}</p>
+        </div>
     `;
-    console.log(markup)
+  
     elements.movieDetailsModal.insertAdjacentHTML('beforeend', markup);
 };
